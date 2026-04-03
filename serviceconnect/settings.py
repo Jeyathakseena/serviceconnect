8615434822c092752abca67a407623a465699608
Jeyathakseena <jeyathakseena1704@gmail.com>
2026-04-03 16:31:29 +0530

    Fixed ALLOWED_HOSTS for new Render URL

--- serviceconnect/settings.py
+++ serviceconnect/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = 'django-insecure-replace-this-with-your-own-secret-key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['serviceconnect-app.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
