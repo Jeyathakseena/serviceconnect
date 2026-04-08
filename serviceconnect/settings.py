f8695e0eb90e24e6286587df53b3c92f82ce174e
Jeyathakseena <jeyathakseena1704@gmail.com>
2026-04-08 16:41:26 +0530

    Production Security Fix

--- serviceconnect/settings.py
+++ serviceconnect/settings.py
@@ -16,12 +16,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-replace-this-with-your-own-secret-key'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-key-for-local')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['serviceconnect-s8q9.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
